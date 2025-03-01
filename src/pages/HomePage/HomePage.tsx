e328cc60ad7a1f2362e34dad8e99ced0e7db2f18
Carlos Gamboa <carlosgamboa@mail.com>
2025-03-01 15:07:16 +0100

    feat: add Vitest

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,11 @@
+import { SearchInput } from "@/components/ui/SearchInput"
 import styles from "./styles.module.css"
 
 export const HomePage = () => {
   return (
     <div className={styles.container}>
       <h1>Home</h1>
+      <SearchInput />
     </div>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
 import { defineConfig } from "vite"
+import { configDefaults } from "vitest/config"
 import react from "@vitejs/plugin-react"
+import path from "node:path"
 
-// https://vite.dev/config/
 export default defineConfig({
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["./test/setup.ts"],
+    exclude: [...configDefaults.exclude, "e2e/*"],
+    css: true,
+    coverage: {
+      enabled: true,
+      include: ["src/components/**/*.tsx", "src/pages/**/*.tsx"],
+      exclude: ["**/*.d.ts", "**/types.ts"],
+      provider: "v8",
+      reporter: ["lcov", "text"],
+      reportsDirectory: "./coverage",
+      all: true,
+    },
+  },
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(process.cwd(), "./src"),
+      "#": path.resolve(process.cwd(), "./test"),
+    },
+  },
 })
